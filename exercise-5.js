bc645691a9cf6b364dbf8a3b549fabe7240ac8de
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-01-23 11:19:11 +0100

    Add files via upload

---
+++ exercise-5.js
@@ -0,0 +1,9 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const { id, name = person.firstName, surname = person.lastName, old = person.age} = person;
+console.log(id, name, surname, old);
