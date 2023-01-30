3a1f16656a2bdc9971daf85c18d65962ac05343a
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-01-30 21:01:35 +0100

    Update exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -10,6 +10,14 @@ const filteredPerson = {
   age: 25,
 };
 
-const json = JSON.stringify(filteredPerson);
+const json = JSON.stringify(person, ['id', 'age']);
 
 console.log(json); // Should return: { id: 1, age: 25 }
+
+// era giusta anche questa come soluzione?
+// const { id, age } = person;
+// const filtered = { id, age };
+
+// const json = JSON.stringify(filtered);
+
+// console.log(json);
